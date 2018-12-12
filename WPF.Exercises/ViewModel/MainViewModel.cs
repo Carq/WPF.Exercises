93b30e32133436b1c6ef3fdee4f234d90863e015
Carq <lciura.pl@gmail.com>
2018-12-12 19:28:40 +0100

    Exercise 2 - solution

--- WPF.Exercises/ViewModel/MainViewModel.cs
+++ WPF.Exercises/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 using System.Collections.ObjectModel;
 using WPF.Exercises.Service;
 using WPF.Exercises.Service.Dto;
@@ -13,8 +14,48 @@ namespace WPF.Exercises.ViewModel
         {
             _fleetService = fleetService;
             Cars = new ObservableCollection<CarDto>(_fleetService.GetAllCars());
+            CleanCommand = new RelayCommand(CleanCars, CanClearCars);
+            LoadCommand = new RelayCommand(Load, CanLoad);
+            AddNewCarCommand = new RelayCommand(AddNewCar);
+        }
+
+        private bool CanClearCars()
+        {
+            return Cars.Count > 0;
+        }
+
+        private void CleanCars()
+        {
+            Cars.Clear();
+        }
+
+        private bool CanLoad()
+        {
+            return Cars.Count == 0;
+        }
+
+        public void Load()
+        {
+            Cars = new ObservableCollection<CarDto>(_fleetService.GetAllCars());
+            RaisePropertyChanged(() => Cars);
+        }
+
+        private void AddNewCar()
+        {
+            Cars.Add(new CarDto
+            {
+                Id = 999,
+                Brand = "Fiat",
+                Model = "126p"
+            });
         }
 
         public ObservableCollection<CarDto> Cars { get; set; }
+
+        public RelayCommand CleanCommand { get; }
+
+        public RelayCommand LoadCommand { get; }
+
+        public RelayCommand AddNewCarCommand { get; }
     }
 }
